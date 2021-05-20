867d6118e83f42897326eadbb5cdf623e0c0e9c0
JosefJ <jelacic.josef@gmail.com>
2021-05-20 10:22:22 +0200

    adjusted Deed status setup; added docs

--- contracts/PWN.sol
+++ contracts/PWN.sol
@@ -7,6 +7,11 @@ import "./PWNDeed.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PWN is Ownable {
+
+    /*----------------------------------------------------------*|
+    |*  # VARIABLES & CONSTANTS DEFINITIONS                     *|
+    |*----------------------------------------------------------*/
+
     using MultiToken for MultiToken.Asset;
 
     PWNDeed public token;
@@ -14,8 +19,12 @@ contract PWN is Ownable {
 
     uint256 public minDuration = 0;
 
-    event NewDeed(uint8 cat, uint256 id, uint256 amount, address tokenAddress, uint256 expiration, uint256 did);
-    event NewOffer(uint8 cat, uint256 amount, address indexed tokenAddress, address indexed lender, uint256 toBePaid, uint256 did, bytes32 offer);
+    /*----------------------------------------------------------*|
+    |*  # EVENTS & ERRORS DEFINITIONS                           *|
+    |*----------------------------------------------------------*/
+
+    event NewDeed(uint8 cat, uint256 id, uint256 amount, address indexed tokenAddress, uint256 expiration, uint256 indexed did);
+    event NewOffer(uint8 cat, uint256 amount, address tokenAddress, address indexed lender, uint256 toBePaid, uint256 indexed did, bytes32 offer);
     event DeedRevoked(uint256 did);
     event OfferRevoked(bytes32 offer);
     event OfferAccepted(uint256 did, bytes32 offer);
@@ -24,18 +33,39 @@ contract PWN is Ownable {
     event MinDurationChange(uint256 minDuration);
 
 
-constructor(
+    /*----------------------------------------------------------*|
+    |*  # CONSTRUCTOR & FUNCTIONS                               *|
+    |*----------------------------------------------------------*/
+
+    /*
+     *  Constructor
+     *  @title PWN
+     *  @dev establishes a connection with other pre-deployed components
+     *  @dev for the set up to work both PWNDeed & PWNVault contracts have to called via `.setPWN(PWN.address)`
+     *  @param _PWND address of the PWNDeed contract - defines Deed tokens
+     *  @param _PWNV address of the PWNVault contract - holds assets
+     */
+    constructor(
         address _PWND,
         address _PWNV
-    ) 
+    )
     Ownable()
     {
         token = PWNDeed(_PWND);
         vault = PWNVault(_PWNV);
     }
-    
-    // add events
-    // sets STATUS =  0
+
+    /*
+     *  newDeed - sets & locks collateral
+     *  @dev for UI integrations is this the function enabling creation of a new Deed token
+     *  @dev Deed status is set to 1
+     *  @param _cat - category of the asset - see { MultiToken.sol }
+     *  @param _id - ID of an ERC721 or ERC1155 token || 0 in case the token doesn't have IDs
+     *  @param _amount - amount of an ERC20 or ERC1155 token || 0 in case of NFTs
+     *  @param _tokenAddress - address of the asset contract
+     *  @param _expiration - unix time stamp in !! seconds !! (not miliseconds returned by JS)
+     *  @returns a Deed ID of the newly created Deed
+     */
     function newDeed(
         uint8   _cat,
         uint256 _id,
@@ -43,16 +73,46 @@ constructor(
         address _tokenAddress,
         uint256 _expiration
     ) external returns (uint256) {
-        require(_cat < 3, "Unknown token type");
+        require(_cat < 3, "Unknown asset type");
         require(_expiration > (block.timestamp + minDuration));
 
-        uint256 did = token.mint(_cat, _amount, _id, _tokenAddress, _expiration, msg.sender);
-        vault.push(token.getDeedAsset(did)); //, "Asset wasn't stored in vault");
+        uint256 did = token.mint(_cat, _id, _amount, _tokenAddress, _expiration, msg.sender);
+        vault.push(token.getDeedAsset(did));
+        token.changeStatus(1, did);
 
         emit NewDeed(_cat, _id, _amount, _tokenAddress, _expiration, did);
         return did;
     }
 
+    /*
+     *  revokeDeed
+     *  @dev through this function the borrower can delete the Deed token given no offer was accepted
+     *  @param _did - Deed ID specifying the concrete Deed
+     */
+    function revokeDeed(
+        uint256 _did
+    ) external {
+        require(msg.sender == token.getBorrower(_did), "The deed doesn't belong to the caller");
+        require(token.getDeedStatus(_did) == 1, "Deed can't be revoked at this stage");
+
+        vault.pull(token.getDeedAsset(_did), msg.sender);
+        token.changeStatus(0, _did);
+        token.burn(_did, msg.sender);
+        emit DeedRevoked(_did);
+    }
+
+    /*
+     *  makeOffer
+     *  @dev this is the function used by lenders to cast their offers
+     *  @dev this function doesn't assume the asset is approved yet for PWNVault
+     *  @dev this function requires lender to have a sufficient balance
+     *  @param _cat - category of the asset - see { MultiToken.sol }
+     *  @param _amount - amount of an ERC20 or ERC1155 token to be offered as credit
+     *  @param _tokenAddress - address of the asset contract
+     *  @param _did - ID of the Deed the offer should be bound to
+     *  @param _toBePaid - amount to be paid back by the borrower
+     *  @returns a hash of the newly created offer
+     */
     function makeOffer(
         uint8 _cat,
         uint256 _amount,
@@ -60,65 +120,73 @@ constructor(
         uint256 _did,
         uint256 _toBePaid
     ) external returns (bytes32) {
-        require(_did <= token.id(), "Contract not found"); //replace with borrower addres present
-        require(token.getDeedStatus(_did) == 0, "Contract can't accept offers");
+        require(token.getDeedStatus(_did) == 1, "Deed not accepting offers");
 
-        bytes32 offer = token.setOffer(_cat, _amount, _tokenAddress, msg.sender, _toBePaid, _did);
+        bytes32 offer = token.setOffer(_cat, _amount, _tokenAddress, msg.sender, _did, _toBePaid);
         emit NewOffer(_cat, _amount, _tokenAddress,  msg.sender, _toBePaid, _did, offer);
 
         return offer;
     }
 
+    /*
+     *  revokeOffer
+     *  @dev this is the function lenders can use to remove their offers on Deeds they are in the stage of getting offers
+     *  @param _offer - identifier of the offer to be revoked
+     */
     function revokeOffer(
         bytes32 _offer
     ) external {
-        require(token.getLender(_offer) == msg.sender, "You are not the lender");
-        require(token.getDeedStatus(token.getDeedID(_offer)) == 0, "Contract already started");
+        require(token.getLender(_offer) == msg.sender, "This address didn't create ths offer");
+        require(token.getDeedStatus(token.getDeedID(_offer)) == 1, "Can only remove offers from open Deeds");
         token.deleteOffer(_offer);
         emit OfferRevoked(_offer);
     }
 
-    function revokeDeed(
-        uint256 _did
-    ) external {
-        require(msg.sender == token.getBorrower(_did), "The deed doesn't belong to the caller");
-        require(token.getDeedStatus(_did) == 0);
-
-        token.burn(_did, msg.sender);
-        emit DeedRevoked(_did);
-    }
-
-
-    // sets STATUS =  1
+    /*
+     *  acceptOffer
+     *  @dev through this function a borrower can accept an existing offer
+     *  @dev a UI should do an off-chain balance check on the lender side to make sure the call won't throw
+     *  @dev Deed status is set to 2
+     *  @param _did - Deed ID specifying the deed being processed
+     *  @param _offer - identifier of the offer to be accepted
+     *  @returns true if successful
+     */
     function acceptOffer(
-        uint256 _did,
         bytes32 _offer
     ) external returns (bool) {
-        require(msg.sender == token.getBorrower(_did), "The deed doesn't belong to the caller");
-        require(token.getDeedStatus(_did) == 0);
-        require(token.getDeedID(_offer) == _did);
+        uint256 did = token.getDeedID(_offer);
+        require(msg.sender == token.getBorrower(did), "The deed doesn't belong to the caller");
+        require(token.getDeedStatus(did) == 1, "Deed can't accept more offers");
 
-        token.setCredit(_did, _offer);
+        token.setCredit(did, _offer);
+        token.changeStatus(2, did);
+
         address lender = token.getLender(_offer);
         vault.pullProxy(token.getOfferAsset(_offer), lender, msg.sender);
 
         MultiToken.Asset memory Deed;
-        Deed.cat = 3;
-        Deed.id = _did;
+        Deed.cat = 2;
+        Deed.id = did;
         Deed.tokenAddress = address(token);
 
         vault.pullProxy(Deed, msg.sender, lender);
-        emit OfferAccepted(_did, _offer);
+        emit OfferAccepted(did, _offer);
 
         return true;
     }
 
-    // sets STATUS =  2
+    /*
+     *  payBack
+     *  @dev the borrower can pay back the funds through this function
+     *  @dev the function assumes the asset (and amount to be paid back) to be returned is approved for PWNVault
+     *  @dev the function assumes the borrower has the full amount to be paid back in their account
+     *  @param _did - Deed ID of the deed being paid back
+     *  @returns true if successful
+     */
     function payBack(uint256 _did) external returns (bool) {
-        require(block.timestamp < token.getExpiration(_did), "Contract expired");
-        require(token.getDeedStatus(_did) == 1);
+        require(token.getDeedStatus(_did) == 2, "Deed doesn't have an accepted offer to be paid back");
 
-        token.changeStatus(2, _did);
+        token.changeStatus(3, _did);
 
         bytes32 offer = token.getAcceptedOffer(_did);
         MultiToken.Asset memory credit = token.getOfferAsset(offer);
@@ -131,19 +199,23 @@ constructor(
         return true;
     }
 
-    // sets STATUS =  2
+    /*
+     *  claim Deed
+     *  @dev The current Deed owner can call this function if the Deed
+     *  @param _did - Deed ID of the deed to be claimed
+     *  @returns true if successful
+     */
     function claimDeed(uint256 _did) external returns (bool) {
-        require(token.balanceOf(msg.sender, _did) == 1, "Unauthorized caller");
-        require(token.getExpiration(_did) < block.timestamp || token.getDeedStatus(_did) == 2, "Contract not yet defaulted");
+        require(token.balanceOf(msg.sender, _did) == 1, "Caller is not the deed owner");
+        require(token.getDeedStatus(_did) >= 3, "Deed can't be claimed yet");
 
-        if (token.getDeedStatus(_did) == 2) {
+        if (token.getDeedStatus(_did) == 3) {
             bytes32 offer = token.getAcceptedOffer(_did);
             MultiToken.Asset memory credit = token.getOfferAsset(offer);
             credit.amount = token.toBePaid(offer);
-        
+
             vault.pull(credit, msg.sender);
-        } else {
-            
+        } else if (token.getDeedStatus(_did) == 4) {
             vault.pull(token.getDeedAsset(_did), msg.sender);
         }
 
@@ -152,9 +224,17 @@ constructor(
         return true;
     }
 
+    /*----------------------------------------------------------*|
+    |*  ## SETUP FUNCTIONS                                      *|
+    |*----------------------------------------------------------*/
+
+    /*
+     *  changeMinDuration
+     *  @dev this function sets a minimal time buffer requirement to the expiration time
+     *  @param _newMinDuration
+     */
     function changeMinDuration(uint256 _newMinDuration) external onlyOwner {
         minDuration = _newMinDuration;
         emit MinDurationChange(_newMinDuration);
     }
-
 }
