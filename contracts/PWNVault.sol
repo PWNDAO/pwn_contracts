f36affd95626908efe9a188407b7c30c2dc17fff
Naim Ashhab <ashhab.naim@gmail.com>
2021-09-29 13:52:31 +0200

    remove unnecessary imports

--- contracts/PWNVault.sol
+++ contracts/PWNVault.sol
@@ -1,8 +1,6 @@
 pragma solidity ^0.8.0;
 
 import "./MultiToken.sol";
-import "./PWN.sol";
-import "./PWNDeed.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 
