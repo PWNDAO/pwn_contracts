2c972518f95ce3c3feea3e647c59a0095bc8671e
Josef Je <me@josef.je>
2021-10-11 10:49:53 +0200

    hardhat config stripped of deploy key import

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,17 +8,17 @@ task("accounts", "Prints the list of accounts", async () => {
     }
 });
 
-const PRIVATE_KEY = require("./keys/PRIVATE.json").key1;
+// const PRIVATE_KEY = require("./keys/PRIVATE.json").key1;
 
 module.exports = {
-    solidity: "0.8.4",
-    networks: {
-        // To deploy to Kovan testnet, uncomment setup bellow & KOVAN_PRIVATE_KEY.
-        // Note you have to have ./.keys/PRIVATE.json file locally.
-
-        rinkeby: {
-            url: `https://rinkeby.infura.io/v3/1ef6c7c90cde4e21b161b48edeb5c8c8`,
-            accounts: [`0x${PRIVATE_KEY}`]
-        }
-    }
+    solidity: "0.8.4"
+    // networks: {
+    //     // To deploy to Kovan testnet, uncomment setup bellow & KOVAN_PRIVATE_KEY.
+    //     // Note you have to have ./.keys/PRIVATE.json file locally.
+    //
+    //     rinkeby: {
+    //         url: `https://rinkeby.infura.io/v3/1ef6c7c90cde4e21b161b48edeb5c8c8`,
+    //         accounts: [`0x${PRIVATE_KEY}`]
+    //     }
+    // }
 };
