722961829b8e51ad9e959201bbcb600e86a7a5db
Josef Je <me@josef.je>
2021-10-11 10:41:53 +0200

    kovan -> rinkeby switch WARN: exposing API

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,17 +8,17 @@ task("accounts", "Prints the list of accounts", async () => {
     }
 });
 
-// const KOVAN_PRIVATE_KEY = require("./.keys/PRIVATE.json").key1;
+const PRIVATE_KEY = require("./keys/PRIVATE.json").key1;
 
 module.exports = {
     solidity: "0.8.4",
     networks: {
         // To deploy to Kovan testnet, uncomment setup bellow & KOVAN_PRIVATE_KEY.
         // Note you have to have ./.keys/PRIVATE.json file locally.
-        //
-        // kovan: {
-        //     url: `https://kovan.infura.io/v3/2e64f610a85f433d83a708df23e6e71f`,
-        //     accounts: [`0x${KOVAN_PRIVATE_KEY}`]
-        // }
+
+        rinkeby: {
+            url: `https://rinkeby.infura.io/v3/1ef6c7c90cde4e21b161b48edeb5c8c8`,
+            accounts: [`0x${PRIVATE_KEY}`]
+        }
     }
 };
