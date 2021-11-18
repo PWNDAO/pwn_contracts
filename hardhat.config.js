1e29f12a0b9b880ec09d1d6994ac1755490f5e9b
Naim Ashhab <ashhab.naim@gmail.com>
2021-11-18 17:58:52 +0100

    add gas reporter hardhat plugin

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,14 @@
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
 const config = require("config");
 
 module.exports = {
     solidity: "0.8.4",
     networks: config.networks,
-    etherscan: config.etherscan
+    etherscan: config.etherscan,
+    gasReporter: {
+        currency: "USD",
+        gasPrice: 150,
+        enabled: true
+    }
 };
