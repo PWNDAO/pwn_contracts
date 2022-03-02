8bc7e6ebfb4a8a3773dd4ea665f67d7171dcd3e4
ashhanai <ashhab.naim@gmail.com>
2022-03-02 09:42:46 -0300

    rename PWNDeed to PWNLoan

--- contracts/PWN.sol
+++ contracts/PWN.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.4;
 
 import "./PWNVault.sol";
-import "./PWNDeed.sol";
+import "./PWNLoan.sol";
 import "@pwnfinance/multitoken/contracts/MultiToken.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -12,7 +12,7 @@ contract PWN is Ownable {
     |*  # VARIABLES & CONSTANTS DEFINITIONS                     *|
     |*----------------------------------------------------------*/
 
-    PWNDeed public deed;
+    PWNLoan public loan;
     PWNVault public vault;
 
     /*----------------------------------------------------------*|
@@ -28,15 +28,15 @@ contract PWN is Ownable {
     /**
      * Constructor
      * @dev Establishes a connection with other pre-deployed components
-     * @dev For the set up to work both PWNDeed & PWNVault contracts have to called via `.setPWN(PWN.address)`
-     * @param _PWND Address of the PWNDeed contract - defines Deed tokens
+     * @dev For the set up to work both PWNLoan & PWNVault contracts have to called via `.setPWN(PWN.address)`
+     * @param _PWNL Address of the PWNLoan contract - defines LOAN tokens
      * @param _PWNV Address of the PWNVault contract - holds assets
      */
     constructor(
-        address _PWND,
+        address _PWNL,
         address _PWNV
     ) Ownable() {
-        deed = PWNDeed(_PWND);
+        loan = PWNLoan(_PWNL);
         vault = PWNVault(_PWNV);
     }
 
@@ -52,25 +52,25 @@ contract PWN is Ownable {
         bytes32 _offerHash,
         bytes calldata _signature
     ) external returns (bool) {
-        deed.revokeOffer(_offerHash, _signature, msg.sender);
+        loan.revokeOffer(_offerHash, _signature, msg.sender);
 
         return true;
     }
 
     /**
-     * createDeed
+     * createLoan
      * @notice Borrower can accept existing signed off-chain offer
      * @dev A UI should do an off-chain balance check on the lender side to make sure the call won't throw
      * @dev Loan asset has to be an ERC20 token, otherwise will transaction fail
-     * @param _offer Offer struct with plain offer data. See { PWNDeed.sol }
+     * @param _offer Offer struct with plain offer data. See { PWNLoan.sol }
      * @param _signature Offer typed struct signed by lender
      * @return True if successful
      */
-    function createDeed(
-        PWNDeed.Offer memory _offer,
+    function createLoan(
+        PWNLoan.Offer memory _offer,
         bytes memory _signature
     ) external returns (bool) {
-        deed.create(_offer, _signature, msg.sender);
+        loan.create(_offer, _signature, msg.sender);
 
         MultiToken.Asset memory collateral = MultiToken.Asset(
             _offer.collateralAddress,
@@ -79,7 +79,7 @@ contract PWN is Ownable {
             _offer.collateralId
         );
 
-        MultiToken.Asset memory loan = MultiToken.Asset(
+        MultiToken.Asset memory loanAsset = MultiToken.Asset(
             _offer.loanAssetAddress,
             MultiToken.Category.ERC20,
             _offer.loanAmount,
@@ -87,27 +87,27 @@ contract PWN is Ownable {
         );
 
         vault.pull(collateral, msg.sender);
-        vault.pushFrom(loan, _offer.lender, msg.sender);
+        vault.pushFrom(loanAsset, _offer.lender, msg.sender);
 
         return true;
     }
 
     /**
-     * createDeedFlexible
+     * createLoanFlexible
      * @notice Borrower can accept existing signed off-chain flexible offer
      * @dev A UI should do an off-chain balance check on the lender side to make sure the call won't throw
      * @dev Loan asset has to be an ERC20 token, otherwise will transaction fail
-     * @param _offer Flexible offer struct with plain flexible offer data. See { PWNDeed.sol }
-     * @param _offerValues Concrete values of a flexible offer set by borrower
+     * @param _offer Flexible offer struct with plain flexible offer data. See { PWNLoan.sol }
+     * @param _offerValues Concrete values of a flexible offer set by borrower. See { PWNLoan.sol }
      * @param _signature Flexible offer typed struct signed by lender
      * @return True if successful
      */
-    function createDeedFlexible(
-        PWNDeed.FlexibleOffer memory _offer,
-        PWNDeed.FlexibleOfferValues memory _offerValues,
+    function createLoanFlexible(
+        PWNLoan.FlexibleOffer memory _offer,
+        PWNLoan.FlexibleOfferValues memory _offerValues,
         bytes memory _signature
     ) external returns (bool) {
-        deed.createFlexible(_offer, _offerValues, _signature, msg.sender);
+        loan.createFlexible(_offer, _offerValues, _signature, msg.sender);
 
         MultiToken.Asset memory collateral = MultiToken.Asset(
             _offer.collateralAddress,
@@ -116,7 +116,7 @@ contract PWN is Ownable {
             _offerValues.collateralId
         );
 
-        MultiToken.Asset memory loan = MultiToken.Asset(
+        MultiToken.Asset memory loanAsset = MultiToken.Asset(
             _offer.loanAssetAddress,
             MultiToken.Category.ERC20,
             _offerValues.loanAmount,
@@ -124,7 +124,7 @@ contract PWN is Ownable {
         );
 
         vault.pull(collateral, msg.sender);
-        vault.pushFrom(loan, _offer.lender, msg.sender);
+        vault.pushFrom(loanAsset, _offer.lender, msg.sender);
 
         return true;
     }
@@ -134,42 +134,42 @@ contract PWN is Ownable {
      * @notice The borrower can pay back the loan through this function
      * @dev The function assumes the asset (and amount to be paid back) to be returned is approved for PWNVault
      * @dev The function assumes the borrower has the full amount to be paid back in their account
-     * @param _did Deed ID of the deed being paid back
+     * @param _loanId LOAN ID of the loan being paid back
      * @return True if successful
      */
-    function repayLoan(uint256 _did) external returns (bool) {
-        deed.repayLoan(_did);
+    function repayLoan(uint256 _loanId) external returns (bool) {
+        loan.repayLoan(_loanId);
 
-        MultiToken.Asset memory loan = deed.getLoan(_did);
-        loan.amount = deed.getLoanRepayAmount(_did);
+        MultiToken.Asset memory loanAsset = loan.getLoan(_loanId);
+        loanAsset.amount = loan.getLoanRepayAmount(_loanId);
 
-        vault.pull(loan, msg.sender);
-        vault.push(deed.getCollateral(_did), deed.getBorrower(_did));
+        vault.pull(loanAsset, msg.sender);
+        vault.push(loan.getCollateral(_loanId), loan.getBorrower(_loanId));
 
         return true;
     }
 
     /**
-     * claimDeed
-     * @dev The current Deed owner can call this function if the Deed is expired or paied back
-     * @param _did Deed ID of the deed to be claimed
+     * claimLoan
+     * @dev The current LOAN owner can call this function if the loan is expired or paied back
+     * @param _loanId LOAN ID of the loan to be claimed
      * @return True if successful
      */
-    function claimDeed(uint256 _did) external returns (bool) {
-        uint8 status = deed.getStatus(_did);
+    function claimLoan(uint256 _loanId) external returns (bool) {
+        uint8 status = loan.getStatus(_loanId);
 
-        deed.claim(_did, msg.sender);
+        loan.claim(_loanId, msg.sender);
 
         if (status == 3) {
-            MultiToken.Asset memory loan = deed.getLoan(_did);
-            loan.amount = deed.getLoanRepayAmount(_did);
+            MultiToken.Asset memory loanAsset = loan.getLoan(_loanId);
+            loanAsset.amount = loan.getLoanRepayAmount(_loanId);
 
-            vault.push(loan, msg.sender);
+            vault.push(loanAsset, msg.sender);
         } else if (status == 4) {
-            vault.push(deed.getCollateral(_did), msg.sender);
+            vault.push(loan.getCollateral(_loanId), msg.sender);
         }
 
-        deed.burn(_did, msg.sender);
+        loan.burn(_loanId, msg.sender);
 
         return true;
     }
