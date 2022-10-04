3dfefd92945e38ee6b4bcd97742d482c490831e8
ashhanai <ashhab.naim@gmail.com>
2022-10-04 12:48:34 +0100

    Implement PWNConfig

---
+++ src/PWNConfig.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.4;
+
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+
+contract PWNConfig is Ownable {
+
+    /// Fee size in basis points
+    /// 100 -> 1%
+    uint16 public fee;
+
+    constructor(uint16 _fee) Ownable() {
+        fee = _fee;
+    }
+
+
+    function setFee(uint16 _fee) external onlyOwner {
+        fee = _fee;
+    }
+
+}
