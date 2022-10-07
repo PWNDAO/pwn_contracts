bc430375bcc6c294ce822cf7d2fe777cf9a07d36
ashhanai <ashhab.naim@gmail.com>
2022-10-07 15:56:09 +0100

    Implement testing tokens

---
+++ test/helpers/token/T20.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+
+contract T20 is ERC20("ERC20", "ERC20") {
+
+	function mint(address owner, uint256 amount) external {
+		_mint(owner, amount);
+	}
+
+	function burn(address owner, uint256 amount) external {
+		_burn(owner, amount);
+	}
+
+}
