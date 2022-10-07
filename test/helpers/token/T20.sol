1928810e24e00c3586196d6a3ae9e3b6fb9e6a52
ashhanai <ashhab.naim@gmail.com>
2022-10-07 17:06:44 +0100

    Update solc version to 0.8.16

--- test/helpers/token/T20.sol
+++ test/helpers/token/T20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity 0.8.16;
 
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
--- test/helpers/token/T721.sol
+++ test/helpers/token/T721.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity 0.8.16;
 
 import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
