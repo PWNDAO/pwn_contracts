8d510b6deb5e731a3248b602e11d497ee691d43a
ashhanai <ashhab.naim@gmail.com>
2022-10-25 17:21:17 -0300

    Add missing code doc

--- src/deployer/PWNDeployer.sol
+++ src/deployer/PWNDeployer.sol
@@ -5,7 +5,10 @@ import "openzeppelin-contracts/contracts/access/Ownable.sol";
 import "openzeppelin-contracts/contracts/utils/Create2.sol";
 
 
-/// TODO: Doc
+/**
+ * @title PWN Deployer
+ * @notice Contract that deploy other PWN protocol contracst with `CREATE2` opcode, to have the same address on a different chains.
+ */
 contract PWNDeployer is Ownable {
 
     constructor(address owner) Ownable() {
@@ -13,12 +16,24 @@ contract PWNDeployer is Ownable {
     }
 
 
-    /// TODO: Doc
+    /**
+     * @notice Deploy new contract with salt.
+     * @dev Set of salts is defined in {PWNContractDeployerSalt.sol}.
+     *      Only deployer owner can call this function.
+     * @param salt Salt used in `CREATE2` call.
+     * @param bytecode Contracts create code encoded with constructor params.
+     * @return Newly deployed contract address.
+     */
     function deploy(bytes32 salt, bytes memory bytecode) external onlyOwner returns (address) {
         return Create2.deploy(0, salt, bytecode);
     }
 
-    /// TODO: Doc
+    /**
+     * @notice Compute address of a contract that would be deployed with given salt.
+     * @param salt Salt used in `CREATE2` call.
+     * @param bytecodeHash Hash of a contracts create code encoded with constructor params.
+     * @return Computed contract address.
+     */
     function computeAddress(bytes32 salt, bytes32 bytecodeHash) external view returns (address) {
         return Create2.computeAddress(salt, bytecodeHash);
     }
