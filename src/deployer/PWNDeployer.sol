d2f5d7365de29d283e375fce753e0a326d82c495
ashhanai <ashhab.naim@gmail.com>
2023-01-26 15:52:53 -0300

    Implement deploy and transfer ownership function on PWNDeployer

--- src/deployer/PWNDeployer.sol
+++ src/deployer/PWNDeployer.sol
@@ -29,6 +29,21 @@ contract PWNDeployer is Ownable {
     }
 
     /**
+     * @notice Deploy new contract with salt and transfer its ownership to the `owner`.
+     * @dev Set of salts is defined in {PWNContractDeployerSalt.sol}.
+     *      Only deployer owner can call this function.
+     *      The newly deployed contract is expected to be `Ownable` to execute the `transferOwnership` function.
+     * @param salt Salt used in `CREATE2` call.
+     * @param owner Address to which the contract ownership is transferred after deployment.
+     * @param bytecode Contracts create code encoded with constructor params.
+     * @return deployedContract Newly deployed contract address.
+     */
+    function deployAndTransferOwnership(bytes32 salt, address owner, bytes memory bytecode) external onlyOwner returns (address deployedContract) {
+        deployedContract = Create2.deploy(0, salt, bytecode);
+        Ownable(deployedContract).transferOwnership(owner);
+    }
+
+    /**
      * @notice Compute address of a contract that would be deployed with given salt.
      * @param salt Salt used in `CREATE2` call.
      * @param bytecodeHash Hash of a contracts create code encoded with constructor params.
--- test/unit/PWNDeployer.t.sol
+++ test/unit/PWNDeployer.t.sol
@@ -63,6 +63,51 @@ contract PWNDeployer_Deploy_Test is PWNDeployerTest {
 
 
 /*----------------------------------------------------------*|
+|*  # DEPLOY AND TRANSFER OWNERSHIP                         *|
+|*----------------------------------------------------------*/
+
+contract PWNDeployer_DeployAndTransferOwnership_Test is PWNDeployerTest {
+
+    address newOwner = makeAddr("newOwner");
+
+
+    function test_shouldFail_whenCallerIsNotOwner() external {
+        vm.expectRevert("Ownable: caller is not the owner");
+        deployer.deployAndTransferOwnership(keccak256("PWNHub"), newOwner, type(PWNHub).creationCode);
+    }
+
+    function test_shouldDeployContract() external {
+        vm.prank(owner);
+        address newAddr = deployer.deployAndTransferOwnership({
+            salt: keccak256("PWNHub"),
+            owner: newOwner,
+            bytecode: abi.encodePacked(type(PWNHub).creationCode, abi.encode(address(0x10)))
+        });
+
+        assertEq(keccak256(newAddr.code), keccak256(type(PWNHub).runtimeCode));
+    }
+
+    function test_shouldTransferOwnerhsip() external {
+        bytes32 salt = keccak256("not just any salt");
+        bytes memory creationCode = abi.encodePacked(type(PWNHub).creationCode, abi.encode(address(0x10)));
+
+        vm.expectCall(
+            Create2.computeAddress(salt, keccak256(creationCode), address(deployer)),
+            abi.encodeWithSignature("transferOwnership(address)", newOwner)
+        );
+
+        vm.prank(owner);
+        deployer.deployAndTransferOwnership({
+            salt: salt,
+            owner: newOwner,
+            bytecode: creationCode
+        });
+    }
+
+}
+
+
+/*----------------------------------------------------------*|
 |*  # COMPUTE ADDRESS                                       *|
 |*----------------------------------------------------------*/
 
