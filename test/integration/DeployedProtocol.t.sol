7ef3f548ec297daf285cf30d83dda8501c0b479b
ashhanai <ashhab.naim@gmail.com>
2023-05-16 11:59:00 +0200

    Refactor deployed protocol test

--- test/integration/DeployedProtocol.t.sol
+++ test/integration/DeployedProtocol.t.sol
@@ -6,56 +6,46 @@ import "forge-std/Test.sol";
 import "@pwn-test/helper/DeploymentTest.t.sol";
 
 
-/*
+contract DeployedProtocolTest is DeploymentTest {
 
-Run tests with this command:
-
-FOUNDRY_PROFILE=deployed forge t -f [mainnet, polygon, goerli, mumbai, local]
-
-*/
-abstract contract DeployedProtocolTest is DeploymentTest {
     bytes32 internal constant PROXY_ADMIN_SLOT = 0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
-}
 
+    function _test_deployedProtocol(string memory urlOrAlias) internal {
+        vm.createSelectFork(urlOrAlias);
+        super.setUp();
 
-contract DeployedProtocol_Addresses_Test is DeployedProtocolTest {
-
-    function test_checkAddresses_Deployer() external {
+        // deployer owner is admin
         assertEq(deployer.owner(), admin);
-    }
 
-    function test_checkAddresses_Config() external {
+        // config admin is admin & owner is dao & feeCollector is feeCollector
         assertEq(vm.load(address(config), PROXY_ADMIN_SLOT), bytes32(uint256(uint160(admin))));
         assertEq(config.owner(), dao);
         assertEq(config.feeCollector(), feeCollector);
-    }
 
-    function test_checkAddresses_Hub() external {
+        // hub owner is admin
         assertEq(hub.owner(), admin);
-    }
 
-}
-
-
-contract DeployedProtocol_Tags_Test is DeployedProtocolTest {
-
-    function test_checkTags_SimpleLoan() external {
+        // simple loan has active loan tag
         assertTrue(hub.hasTag(address(simpleLoan), PWNHubTags.ACTIVE_LOAN));
-    }
 
-    function test_checkTags_SimpleOffer() external {
+        // simple loan simple offer has simple loan terms factory & loan offer tags
         assertTrue(hub.hasTag(address(simpleLoanSimpleOffer), PWNHubTags.SIMPLE_LOAN_TERMS_FACTORY));
         assertTrue(hub.hasTag(address(simpleLoanSimpleOffer), PWNHubTags.LOAN_OFFER));
-    }
 
-    function test_checkTags_ListOffer() external {
+        // simple loan list offer has simple loan terms factory & loan offer tags
         assertTrue(hub.hasTag(address(simpleLoanListOffer), PWNHubTags.SIMPLE_LOAN_TERMS_FACTORY));
         assertTrue(hub.hasTag(address(simpleLoanListOffer), PWNHubTags.LOAN_OFFER));
-    }
 
-    function test_checkTags_SimpleRequest() external {
+        // simple loan simple request has simple loan terms factory & loan request tags
         assertTrue(hub.hasTag(address(simpleLoanSimpleRequest), PWNHubTags.SIMPLE_LOAN_TERMS_FACTORY));
         assertTrue(hub.hasTag(address(simpleLoanSimpleRequest), PWNHubTags.LOAN_REQUEST));
+
     }
+
+
+    // function test_deployedProtocol_ethereum() external { _test_deployedProtocol("mainnet"); }
+    // function test_deployedProtocol_polygon() external { _test_deployedProtocol("polygon"); }
+    function test_deployedProtocol_goerli() external { _test_deployedProtocol("goerli"); }
+    // function test_deployedProtocol_mumbai() external { _test_deployedProtocol("mumbai"); }
 
 }
