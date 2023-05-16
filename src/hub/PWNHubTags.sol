1a69207b852a9e5c26140b7c05a8e84986e7507a
ashhanai <ashhab.naim@gmail.com>
2023-05-16 15:43:08 +0200

    Return pwn hub tags order to have same bytecode with deployed contracts

--- src/hub/PWNHubTags.sol
+++ src/hub/PWNHubTags.sol
@@ -11,9 +11,9 @@ library PWNHubTags {
     /// @dev Address can be used as a loan terms factory for creating simple loans.
     bytes32 internal constant SIMPLE_LOAN_TERMS_FACTORY = keccak256("PWN_SIMPLE_LOAN_TERMS_FACTORY");
 
-    /// @dev Address can revoke loan offer nonces.
-    bytes32 internal constant LOAN_OFFER = keccak256("PWN_LOAN_OFFER");
     /// @dev Address can revoke loan request nonces.
     bytes32 internal constant LOAN_REQUEST = keccak256("PWN_LOAN_REQUEST");
+    /// @dev Address can revoke loan offer nonces.
+    bytes32 internal constant LOAN_OFFER = keccak256("PWN_LOAN_OFFER");
 
 }
