1dd111858ae7de5edc3bea2071c7df5b31c09644
ashhanai <ashhab.naim@gmail.com>
2023-07-03 18:40:18 +0100

    Deploy to Cronost testnet and Mantle testnet

--- test/integration/DeployedProtocol.t.sol
+++ test/integration/DeployedProtocol.t.sol
@@ -96,5 +96,7 @@ contract DeployedProtocolTest is DeploymentTest {
     function test_deployedProtocol_polygon() external { _test_deployedProtocol("polygon"); }
     function test_deployedProtocol_goerli() external { _test_deployedProtocol("goerli"); }
     function test_deployedProtocol_mumbai() external { _test_deployedProtocol("mumbai"); }
+    function test_deployedProtocol_cronos_testnet() external { _test_deployedProtocol("cronos_testnet"); }
+    function test_deployedProtocol_mantle_testnet() external { _test_deployedProtocol("mantle_testnet"); }
 
 }
