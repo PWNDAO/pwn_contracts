ab01511d595f252c4adabe58a1ce09d05d09595b
ashhanai <ashhab.naim@gmail.com>
2023-08-16 17:30:06 +0100

    Deploy to Base Goerli

--- test/integration/DeployedProtocol.t.sol
+++ test/integration/DeployedProtocol.t.sol
@@ -98,6 +98,7 @@ contract DeployedProtocolTest is DeploymentTest {
     function test_deployedProtocol_cronos() external { _test_deployedProtocol("cronos"); }
 
     function test_deployedProtocol_goerli() external { _test_deployedProtocol("goerli"); }
+    function test_deployedProtocol_base_goerli() external { _test_deployedProtocol("base_goerli"); }
     function test_deployedProtocol_cronos_testnet() external { _test_deployedProtocol("cronos_testnet"); }
     function test_deployedProtocol_mantle_testnet() external { _test_deployedProtocol("mantle_testnet"); }
 
