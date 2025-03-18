fcc07c5febae73fc93b1d4d169ea6f34c627f80c
ashhanai <ashhab.naim@gmail.com>
2025-03-18 17:32:54 +0100

    test: UniswapV3 lib

---
+++ test/harness/UniswapV3Harness.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.16;
+
+import { UniswapV3 } from "pwn/loan/lib/UniswapV3.sol";
+
+
+contract UniswapV3Harness {
+
+    function getLPValue(
+        UniswapV3.Config memory config,
+        uint256 tokenId,
+        bool token0Denominator
+    ) external view returns (uint256 value, address denominator) {
+        return UniswapV3.getLPValue(config, tokenId, token0Denominator);
+    }
+
+}
