f831a514c02c55bbbb76f9d7bd7388ed6ee668c4
ashhanai <ashhab.naim@gmail.com>
2025-05-15 10:22:10 +0200

    refactor: rename lender direct repayment hook

--- src/loan/hook/lender/repayment/PWNDirectLenderRepaymentHook.sol
+++ src/loan/hook/lender/repayment/PWNDirectLenderRepaymentHook.sol
@@ -6,11 +6,14 @@ import { MultiToken } from "MultiToken/MultiToken.sol";
 import { IPWNLenderRepaymentHook, LENDER_REPAYMENT_HOOK_RETURN_VALUE } from "pwn/loan/hook/lender/repayment/IPWNLenderRepaymentHook.sol";
 
 
-contract PWNLenderDirectRepaymentHook is IPWNLenderRepaymentHook {
+contract PWNDirectLenderRepaymentHook is IPWNLenderRepaymentHook {
     using MultiToken for address;
     using MultiToken for MultiToken.Asset;
 
-    error DataMustBeEmpty();
+    error LenderZeroAddress();
+    error CreditZeroAddress();
+    error RepaymentZero();
+    error DataNotEmpty();
 
     function onLoanRepaid(
         address lender,
@@ -18,7 +21,11 @@ contract PWNLenderDirectRepaymentHook is IPWNLenderRepaymentHook {
         uint256 repayment,
         bytes calldata lenderData
     ) external returns (bytes32) {
-        if (lenderData.length != 0) revert DataMustBeEmpty();
+        if (lender == address(0)) revert LenderZeroAddress();
+        if (creditAddress == address(0)) revert CreditZeroAddress();
+        if (repayment == 0) revert RepaymentZero();
+        if (lenderData.length != 0) revert DataNotEmpty();
+
         creditAddress.ERC20(repayment).transferAssetFrom(address(this), lender);
         return LENDER_REPAYMENT_HOOK_RETURN_VALUE;
     }
